4393c86d985c4012d832610e6c2f6c523df3fb4b
Sivaji Meenugu <shivajimeenugu@gmail.com>
2023-02-25 11:36:08 +0530

    completed state mgmt in controls

--- lib/helios_web/live/controls_live.ex
+++ lib/helios_web/live/controls_live.ex
@@ -94,9 +94,9 @@ defmodule HeliosWeb.ControlsLive do
           </div>
           <div class="w-9/12">
               <form phx-change="slider">
-                <div class=""><input name="slider1" value={"#{@slider1}"} id="default-range" type="range"  class="w-10/12 h-3  rounded appearance-none cursor-pointer bg-yellow-200"></div>
-                <div class=""><input name="slider2" value={"#{@slider2}"} id="default-range" type="range"  class="w-10/12 h-3  rounded-lg appearance-none cursor-pointer bg-gray-100"></div>
-                <div class=""><input name="slider3" value={"#{@slider3}"} id="default-range" type="range"  class="w-10/12 h-3  rounded-lg appearance-none cursor-pointer bg-blue-200"></div>
+                <div class=""><input name="slider1" value={"#{@slider1}"} id="slider1" type="range"  class="w-10/12 h-3  rounded appearance-none cursor-pointer bg-yellow-200"></div>
+                <div class=""><input name="slider2" value={"#{@slider2}"} id="slider2" type="range"  class="w-10/12 h-3  rounded-lg appearance-none cursor-pointer bg-gray-100"></div>
+                <div class=""><input name="slider3" value={"#{@slider3}"} id="slider3" type="range"  class="w-10/12 h-3  rounded-lg appearance-none cursor-pointer bg-blue-200"></div>
               </form>
           </div>
 
@@ -119,12 +119,12 @@ defmodule HeliosWeb.ControlsLive do
           <div class="flex flex-col w-8/12 ">
               <div class="flex items-center">
                   <div class="flex items-center">
-                      <select name="sent_name" class="hselect font-['heliosfont']"  id="" >
-                          <option value="Choose Scent">Choose Scents</option>
-                          <option value="Lavender" >Lavender</option>
-                          <option value="Lime" selected>Lime</option>
-                          <option value="Rose">Rose</option>
-                          <option value="Salty">Salty</option>
+                      <select name="sent_name" class="hselect font-['heliosfont']"  id="sent_name" >
+                          <option value="Choose Scent" >Choose Scents</option>
+                          <option  value="Lavender"  selected = {if @sent_name == "Lavender" do true else false end} > Lavender </option>
+                          <option value="Lime" selected = {if @sent_name == "Lime" do true else false end} >Lime</option>
+                          <option value="Rose" selected = {if @sent_name == "Rose" do true else false end} >Rose</option>
+                          <option value="Salty" selected = {if @sent_name == "Salty" do true else false end}>Salty</option>
                       </select>
                       <i class="fa fa-angle-down pt-2 ml-1"></i>
                       <!-- <i class="fa fa-angle-up pt-2 ml-1"></i> -->
@@ -139,19 +139,19 @@ defmodule HeliosWeb.ControlsLive do
               <div class="font-['heliosfont']">Time</div>
               <div class="rounded-xl w-fit border border-2 border-yellow-300 ">
                   <div class="radio-toolbar p-2 flex flex-col ">
-                      <input class="" type="radio" id="radio1sec" name="sent_time" value="1" >
+                      <input class="" type="radio" id="radio1sec" name="sent_time" value="1" checked = {if @sent_time == "1" do true else false end}>
                       <label class="rounded pl-1 pr-1 pt-1 items-center font-['heliosfont'] flex" for="radio1sec">1 Sec</label>
 
-                      <input type="radio" id="radio2sec" name="sent_time" value="2">
+                      <input type="radio" id="radio2sec" name="sent_time" value="2" checked = {if @sent_time == "2" do true else false end}>
                       <label class="rounded pl-1 pr-1 pt-1 items-center font-['heliosfont'] flex" for="radio2sec">2 Sec</label>
 
-                      <input type="radio" id="radio3sec" name="sent_time" value="3">
+                      <input type="radio" id="radio3sec" name="sent_time" value="3" checked = {if @sent_time == "3" do true else false end}>
                       <label class="rounded pl-1 pr-1 pt-1 items-center font-['heliosfont'] flex" for="radio3sec">3 sec</label>
 
-                      <input type="radio" id="radio4sec" name="sent_time" value="4">
+                      <input type="radio" id="radio4sec" name="sent_time" value="4" checked = {if @sent_time == "4" do true else false end}>
                       <label class="rounded pl-1 pr-1 pt-1 items-center font-['heliosfont'] flex" for="radio4sec">4 sec</label>
 
-                      <input type="radio" id="radio5sec" name="sent_time" value="5">
+                      <input type="radio" id="radio5sec" name="sent_time" value="5" checked = {if @sent_time == "5" do true else false end}>
                       <label class="rounded pl-1 pr-1 pt-1 items-center font-['heliosfont'] flex" for="radio5sec">5 sec</label>
 
                   </div>
@@ -174,7 +174,7 @@ defmodule HeliosWeb.ControlsLive do
           <div class="pt-3">
           <form phx-change="fan">
               <label class=" relative inline-flex items-center mr-5 cursor-pointer">
-                  <input type="checkbox" name="fan" class="sr-only peer" >
+                  <input type="checkbox" name="fan" class="sr-only peer" checked = {if @fan == "on" do true else false end} >
                   <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
                   <!-- <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Green</span> -->
                 </label>
@@ -302,7 +302,7 @@ defmodule HeliosWeb.ControlsLive do
           <div class="items-center  w-6/12 ml-5">
           <form phx-change="wifi">
               <label class="  relative inline-flex items-center mr-5 cursor-pointer">
-                  <input name="wifi" type="checkbox" class="sr-only peer" checked>
+                  <input name="wifi" type="checkbox" class="sr-only peer" checked = {if @wifi == "on" do true else false end}>
                   <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
                   <!-- <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Green</span> -->
                 </label>
